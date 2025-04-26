21e6f67460fb601082a2d388da3ad26cac7edec2
Sauerboy <asauer31@gatech.edu>
2025-04-26 13:11:36 -0400

    randomly plays songs at random speeds

--- Song.py
+++ Song.py
@@ -3,8 +3,16 @@ from pydub import AudioSegment
 from pydub.playback import play
 from pydub import effects
 import ffmpeg
+import random
 
-wiiSound=AudioSegment.from_wav('/Users/arielsauer/Downloads/WiiSound.wav')
-oofSound=AudioSegment.from_mp3('/Users/arielsauer/Downloads/oof.mp3')
+sound_list=[AudioSegment.from_wav('sounds/WiiSound.wav'),
+           AudioSegment.from_mp3('sounds/oof.mp3'),
+           AudioSegment.from_mp3('sounds/spongebob.mp3'),
+           AudioSegment.from_mp3('sounds/onepiece.mp3'),
+           AudioSegment.from_mp3('sounds/deltaco.mp3'),
+           AudioSegment.from_mp3('sounds/mario.mp3')]
 
-play(oofSound)
+dur_str=input("This game will randomly select a sound and play it at a random speed each round. How many rounds would you like to play? Please enter a number:     ")
+dur=int(dur_str)
+for i in range(dur):
+    play(sound_list[random.randint(0, len(sound_list)-1)].speedup(random.uniform(.5,2)))
